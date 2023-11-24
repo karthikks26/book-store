a668ec037aab2b4dfbeac7255bd7c29cc00af330
user pc <kskarthik728@gmail.com>
2023-11-24 14:04:47 +0530

    c

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SnackbarProvider>
-      <BrowserRouter>
+      <HashRouter>
         {" "}
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </SnackbarProvider>
   </React.StrictMode>
 );
