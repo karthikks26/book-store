fd4e7aa360fa046a5ac82255ab5b9e8be49cd9c8
karthikks26 <132473895+karthikks26@users.noreply.github.com>
2023-11-26 00:47:16 +0530

    Update CreateBook.jsx

--- src/pages/CreateBook.jsx
+++ src/pages/CreateBook.jsx
@@ -40,7 +40,7 @@ const CreateBook = () => {
       <BackButton />
       <h1 className="text-3xl my-4">Create Book</h1>
       {loading ? <Loader /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl max-w-[600px] p-4 mx-auto">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
           <input
